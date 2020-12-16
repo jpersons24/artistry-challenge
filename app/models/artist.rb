a1e69e6a1bfdc65216cb553a05590574752c50c6
Jakob Persons <jakobep24@gmail.com>
2020-12-16 17:14:57 -0500

    initial build

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ApplicationRecord
+   has_many :artist_instruments
+   has_many :instruments, through: :artist_instruments
+
+   def instruments_played
+      self.instruments
+   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,27 @@
+
+puts "destroying everything"
+Artist.destroy_all
+Instrument.destroy_all
+ArtistInstrument.destroy_all
+
+
+puts "Discovering artists"
+
 Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
 Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
 Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
 Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
 Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
 
+
+puts "creating instruments"
 Instrument.create(name: "Guitar", classification: "Strings")
 Instrument.create(name: "Violin", classification: "Strings")
 Instrument.create(name: "Flute", classification: "Woodwind")
 Instrument.create(name: "Xylophone", classification: "Percussion")
+
+
+puts "creating artist instruments"
+15.times do 
+   ArtistInstrument.create(artist_id: Artist.all.sample.id, instrument_id: Instrument.all.sample.id)
+end
